7ac1603e428f657cb97cff2f42499d764b707b66
Maurício Junior <junior9771@gmail.com>
2019-12-25 09:14:11 -0300

    add help command

--- app/Application/CreateProduct.php
+++ app/Application/CreateProduct.php
@@ -4,7 +4,22 @@
 namespace App\Application;
 
 
+use Longman\TelegramBot\Request;
+
 class CreateProduct
 {
+    private $telegramRequest;
 
+    public function __construct(Request $telegramRequest)
+    {
+        $this->telegramRequest = $telegramRequest;
+    }
+
+    public function perform()
+    {
+        $this->telegramRequest::sendMessage([
+            'chat_id' => '462914579',
+            'text' => 'Qual o nome do seu produto?'
+        ]);
+    }
 }
--- app/Application/Help.php
+++ app/Application/Help.php
@@ -3,8 +3,32 @@
 
 namespace App\Application;
 
+use Longman\TelegramBot\Request;
 
 class Help
 {
+    private $request;
 
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function perform()
+    {
+        $message = "Olá, eu posso te ajudar em suas vendas e fidelizar clientes. " .
+            "Se você é novo por aqui [leia nosso manual](http://www.google.com/)." .
+            "\nSempre que precisar de ajuda digite o comando /ajuda" .
+            "\n\nComo eu posso te ajudar hoje?".
+            "\n\n/configurarpagamento configurar método de pagamento.".
+            "\n/criarproduto criar um produto.".
+            "\n/venderproduto vender um produto".
+            "\n/contato entre em contato";
+
+        $this->request::sendMessage([
+            'chat_id' => '462914579',
+            'parse_mode' => 'markdown',
+            'text' => $message
+        ]);
+    }
 }
